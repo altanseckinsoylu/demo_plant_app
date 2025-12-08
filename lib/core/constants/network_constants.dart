7d8e25eaebd10d8824a56af316248a152f95760e
Altan Seçkin Soylu <altanseckinsoylu@gmail.com>
2025-12-08 04:24:37 +0300

    .env eklendi

--- lib/core/constants/network_constants.dart
+++ lib/core/constants/network_constants.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class NetworkConstants {
-  static const String baseUrl = 'https://dummy-api-jtg6bessta-ey.a.run.app';
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 15000;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:plant_app/core/init/di_container.dart' as di;
 import 'package:plant_app/core/routes/app_router.dart';
 import 'package:plant_app/core/themes/app_light_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await di.init();
   runApp(MyApp());
 }
